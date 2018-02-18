449e0245e4518dae5e2c1c2e77af35e4dc42eb29
Mesteche <whitefoxproduction@gmail.com>
2018-02-18 18:34:51 +0100

    setup Counter.js

---
+++ src/Counter.js
@@ -0,0 +1,4 @@
+/** @jsx createElement */
+const Counter = ({ createElement }) => <button>Clicked 0 times</button>
+
+export default Counter
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
 import { createElement } from 'react'
 import { render } from 'react-dom'
 import Title from './Title'
+import Counter from './Counter'
 
 render(
-  <Title createElement={createElement} title="Hello world!" />,
+  <div>
+    <Title createElement={createElement} title="Hello world!" />
+    <Counter createElement={createElement} />
+  </div>,
   document.getElementById('root'),
 )
