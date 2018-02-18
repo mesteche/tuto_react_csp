cf4d35dcda914c3183d6110f2887ea8760b95453
Mesteche <whitefoxproduction@gmail.com>
2018-02-18 20:49:32 +0100

    update state management

--- src/index.js
+++ src/index.js
@@ -11,12 +11,56 @@ const root = document.getElementById('root')
 const asyncRender = DOMNode => vDOM =>
   new Promise(resolve => render(vDOM, DOMNode, () => resolve(vDOM)))
 
-const stateReducer = (state, actions) => state
+// prettier-ignore
+const counterReducer = (state = 0, actions = {}) =>
+  Object.entries(actions).reduce(
+    (state, [key, payload]) =>
+      key === 'increment'  ? state + (payload || 0) :
+      key === 'decrement'  ? state - (payload || 0) :
+      key === 'reset'      ? 0                      :
+      state,
+    state
+  )
+// prettier-ignore
+const stateReducer = (
+  state = {
+    Counter1: counterReducer(),
+    Counter2: counterReducer(),
+    sync: false,
+  },
+  actions = {}
+) => Object.entries(actions).reduce(
+  (state, [key, payload]) =>
+    key === 'Counter1'
+      ? { ...state, [key]: counterReducer(state[key], payload) } :
+    key === 'Counter2'
+      ? { ...state, [key]: counterReducer(state[key], payload) } :
+    key === 'sync'
+      ? { ...state, [key]: !state[key] }                         :
+    state,
+  state,
+)
 
 const mapStateToProps = ({ Counter1, Counter2, sync }) => ({
   createElement,
-  Counter1: () => null,
-  Counter2: () => null,
+  Counter1: ({ label, ...props }) =>
+    Counter({
+      createElement,
+      label,
+      value: Counter1,
+      increment: () => {},
+      decrement: () => {},
+      reset: () => {},
+    }),
+  Counter2: ({ label, ...props }) =>
+    Counter({
+      createElement,
+      label,
+      value: Counter2,
+      increment: () => {},
+      decrement: () => {},
+      reset: () => {},
+    }),
   toggleSync: () => {},
 })
 
@@ -36,4 +80,4 @@ pipe(
  * On envoie quelque chose sur notre channel
  * histoire qu'il lance un premier rendu
  **/
-put(appIn, null)
+put(appIn)
