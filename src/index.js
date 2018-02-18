ad01826d5ea8e89442b2dd98d53bbf93193b9328
Mesteche <whitefoxproduction@gmail.com>
2018-02-18 20:44:44 +0100

    add embryo of state management

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
 /** @jsx createElement */
 import { createElement } from 'react'
 import { render } from 'react-dom'
-import { map, filter, channel, put } from './toolbox/csp'
 import { pipe } from './toolbox/fp'
+import { map, reduce, filter, channel, put } from './toolbox/csp'
+import App from './Components/App'
+import Counter from './Components/Counter'
 
 const root = document.getElementById('root')
 
 const asyncRender = DOMNode => vDOM =>
   new Promise(resolve => render(vDOM, DOMNode, () => resolve(vDOM)))
 
-const App = ({ createElement, message }) => <div>{message}</div>
+const stateReducer = (state, actions) => state
+
+const mapStateToProps = ({ Counter1, Counter2, sync }) => ({
+  createElement,
+  Counter1: () => null,
+  Counter2: () => null,
+  toggleSync: () => {},
+})
 
 const appIn = channel()
-// prettier-ignore
 pipe(
+  reduce(stateReducer, {
+    Counter1: 0,
+    Counter2: 0,
+    sync: false,
+  }),
+  map(mapStateToProps),
   map(App),
   map(asyncRender(root)),
   filter(() => false),
 )(appIn)
-
-put(appIn, { createElement, message: 'Hello world!' })
-put(appIn, { createElement, message: 'Hello new world!' })
+/**
+ * On envoie quelque chose sur notre channel
+ * histoire qu'il lance un premier rendu
+ **/
+put(appIn, null)
