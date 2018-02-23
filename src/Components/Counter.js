7dc6a0392530f9ee8402cee07c00193fd9d7a2ab
Mesteche <whitefoxproduction@gmail.com>
2018-02-23 16:27:47 +0100

    give inline styles to the counter component

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,7 +1,9 @@
 /** @jsx createElement */
+
 const wheel = (increment, decrement) => ({ deltaY }) => {
   deltaY < 0 ? increment() : deltaY > 0 && decrement()
 }
+
 const Counter = ({
   createElement,
   label,
@@ -10,8 +12,32 @@ const Counter = ({
   decrement,
   reset,
 }) => (
-  <button onClick={reset} onWheel={wheel(increment, decrement)}>
+  <button
+    style={style({ value })}
+    onClick={reset}
+    onWheel={wheel(increment, decrement)}>
     {label}: {value}
   </button>
 )
+
+const style = ({ value }) => ({
+  padding: 10,
+  width: 150,
+  height: 150,
+  color: '#000',
+  textShadow: `  0px  1px 2px #fff,
+                 1px  0px 2px #fff,
+                -1px  0px 2px #fff,
+                 0px -1px 2px #fff`,
+  fontWeight: 'bold',
+  backgroundColor: '#EEE',
+  background: `radial-gradient(
+    ellipse at center,
+    #ff0080 0%,
+    #ff8c00 ${Math.abs(value) % 100 - 50}%,
+    #40e0d0 ${Math.abs(value) % 100}%
+  )`,
+  borderRadius: '50%',
+})
+
 export default Counter
