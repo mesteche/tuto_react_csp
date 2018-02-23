380e69c0adae4bb2c0f7dd0d8107be6963cfa52e
Mesteche <whitefoxproduction@gmail.com>
2018-02-23 18:04:40 +0100

    replace inline styles with <style/> components

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -13,31 +13,36 @@ const Counter = ({
   reset,
 }) => (
   <button
-    style={style({ value })}
+    className={`button`}
     onClick={reset}
     onWheel={wheel(increment, decrement)}>
+    <style>{style({ value })}</style>
     {label}: {value}
   </button>
 )
 
-const style = ({ value }) => ({
-  padding: 10,
-  width: 150,
-  height: 150,
-  color: '#000',
-  textShadow: `  0px  1px 2px #fff,
-                 1px  0px 2px #fff,
+const style = ({ value }) => `
+.button::-moz-focus-inner {
+  border: 0;
+}
+.button {
+  padding: 10px;
+  width: 150px;
+  height: 150px;
+  color: #000;
+  text-shadow:  0px  1px 2px #fff,
+                1px  0px 2px #fff,
                 -1px  0px 2px #fff,
-                 0px -1px 2px #fff`,
-  fontWeight: 'bold',
-  backgroundColor: '#EEE',
-  background: `radial-gradient(
+                0px -1px 2px #fff;
+  font-weight: bold;
+  background-color: #EEE;
+  background: radial-gradient(
     ellipse at center,
     #ff0080 0%,
     #ff8c00 ${Math.abs(value) % 100 - 50}%,
     #40e0d0 ${Math.abs(value) % 100}%
-  )`,
-  borderRadius: '50%',
-})
+  );
+  border-radius: 50%;
+}`
 
 export default Counter
