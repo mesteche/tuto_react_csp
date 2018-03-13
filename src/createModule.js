b408f6c32641759ff98bebd30b85da8b1d009182
Mesteche <whitefoxproduction@gmail.com>
2018-03-13 18:33:53 +0100

    adapt createModule to handle dependencies

--- src/createModule.js
+++ src/createModule.js
@@ -1,16 +1,47 @@
-import { channel, reduce, map, put } from './toolbox/csp'
+import { channel, reduce, map, any, put } from './toolbox/csp'
 import { pipe } from './toolbox/fp'
+
+const modulesMapper = modules =>
+  Object.entries(modules).reduce(
+    (descriptors, [name, module, inChan = channel()]) => [
+      ...descriptors,
+      {
+        name,
+        module: pipe(module, map(c => ({ [name]: c })))(inChan),
+        dispatch: action => put(inChan, action),
+      },
+    ],
+    [],
+  )
 
 const createModule = Component => ({
   reducer = () => ({}),
   mapStoreToProps = () => x => x,
+  dependencies = {},
   autorun = true,
-}) => (inChan = channel()) =>
-  (!autorun || put(inChan)) &&
-  pipe(
-    reduce(reducer),
-    map(mapStoreToProps(action => put(inChan, action))),
-    map(state => props => Component({ ...state, ...props })),
-  )(inChan)
+}) => (inChan = channel()) => {
+  const modules = modulesMapper(dependencies)
+
+  return (
+    (!autorun || put(inChan)) &&
+    pipe(
+      any(modules.map(m => m.module)),
+      reduce(reducer),
+      map(
+        mapStoreToProps(
+          action => put(inChan, action),
+          modules.reduce(
+            (m, { name, dispatch }) => ({
+              ...m,
+              [name]: dispatch,
+            }),
+            {},
+          ),
+        ),
+      ),
+      map(state => props => Component({ ...state, ...props })),
+    )(inChan)
+  )
+}
 
 export default createModule
