3ea2199e5b1b2e56ce237a68de454963deb7e21c
Mesteche <whitefoxproduction@gmail.com>
2018-03-13 18:54:20 +0100

    simplify createModule

--- src/createModule.js
+++ src/createModule.js
@@ -1,17 +1,18 @@
 import { channel, reduce, map, any, put } from './toolbox/csp'
 import { pipe } from './toolbox/fp'
 
+const initModule = (module, name, inChan) =>
+  pipe(module, map(c => ({ [name]: c })))(inChan)
+
+const createDispatcher = inChan => action => put(inChan, action)
+
 const modulesMapper = modules =>
   Object.entries(modules).reduce(
-    (descriptors, [name, module, inChan = channel()]) => [
-      ...descriptors,
-      {
-        name,
-        module: pipe(module, map(c => ({ [name]: c })))(inChan),
-        dispatch: action => put(inChan, action),
-      },
-    ],
-    [],
+    ({ modules, dispatchers }, [name, module, inChan = channel()]) => ({
+      modules: [...modules, initModule(module, name, inChan)],
+      dispatchers: { ...dispatchers, [name]: createDispatcher(inChan) },
+    }),
+    { modules: [] },
   )
 
 const createModule = Component => ({
@@ -20,25 +21,14 @@ const createModule = Component => ({
   dependencies = {},
   autorun = true,
 }) => (inChan = channel()) => {
-  const modules = modulesMapper(dependencies)
+  const { modules, dispatchers } = modulesMapper(dependencies)
 
   return (
     (!autorun || put(inChan)) &&
     pipe(
-      any(modules.map(m => m.module)),
+      any(modules),
       reduce(reducer),
-      map(
-        mapStoreToProps(
-          action => put(inChan, action),
-          modules.reduce(
-            (m, { name, dispatch }) => ({
-              ...m,
-              [name]: dispatch,
-            }),
-            {},
-          ),
-        ),
-      ),
+      map(mapStoreToProps(createDispatcher(inChan), dispatchers)),
       map(state => props => Component({ ...state, ...props })),
     )(inChan)
   )
