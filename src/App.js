7ea01cdaca1abcd3d63ed7c8c95dc398a373e1c7
Mesteche <whitefoxproduction@gmail.com>
2018-03-13 18:44:37 +0100

    adapt App to use counter module

--- src/App.js
+++ src/App.js
@@ -1,80 +1,56 @@
 import createModule from './createModule'
 import App from './Components/App'
 
-import Counter from './Components/Counter'
+import Counter, { handlers } from './Counter'
 
-// prettier-ignore
-const counterReducer = (state = 0, actions = {}) =>
-Object.entries(actions).reduce(
-  (state, [key, payload]) =>
-    key === 'increment'  ? state + (payload || 0) :
-    key === 'decrement'  ? state - (payload || 0) :
-    key === 'reset'      ? 0                      :
-    state,
-  state
-)
+const syncDispatch = dispatchers => action =>
+  Object.entries(dispatchers).forEach(([, d]) => d(action))
+
 // prettier-ignore
 const reducer = (
-state = {
-  Counter1: counterReducer(),
-  Counter2: counterReducer(),
-  sync: false,
-},
-actions = {}
+  state = {
+    Counter1: () => null,
+    Counter2: () => null,
+    sync: false,
+  },
+  actions = {},
 ) => Object.entries(actions).reduce(
-(state, [key, payload]) =>
-  key === 'Counter1'
-    ? { ...state, [key]: counterReducer(state[key], payload) } :
-  key === 'Counter2'
-    ? { ...state, [key]: counterReducer(state[key], payload) } :
-  key === 'sync'
-    ? { ...state, [key]: !state[key] }                         :
+  (state, [key, payload]) =>
+    key.startsWith('Counter') ? { ...state, [key]: payload }      :
+    key === 'sync'            ? { ...state, [key]: !state[key] }  :
+    state,
   state,
-state,
 )
 
-const mapStoreToProps = dispatch => ({ Counter1, Counter2, sync }) => ({
-  Counter1: props =>
-    Counter({
-      ...props,
-      value: Counter1,
-      increment: () =>
-        dispatch({
-          Counter1: { increment: 1 },
-          ...(sync ? { Counter2: { increment: 1 } } : {}),
-        }),
-      decrement: () =>
-        dispatch({
-          Counter1: { decrement: 1 },
-          ...(sync ? { Counter2: { decrement: 1 } } : {}),
-        }),
-      reset: () =>
-        dispatch({
-          Counter1: { reset: null },
-          ...(sync ? { Counter2: { reset: null } } : {}),
-        }),
-    }),
-  Counter2: props =>
-    Counter({
-      ...props,
-      value: Counter2,
-      increment: () =>
-        dispatch({
-          Counter2: { increment: 1 },
-          ...(sync ? { Counter1: { increment: 1 } } : {}),
-        }),
-      decrement: () =>
-        dispatch({
-          Counter2: { decrement: 1 },
-          ...(sync ? { Counter1: { decrement: 1 } } : {}),
-        }),
-      reset: () =>
-        dispatch({
-          Counter2: { reset: null },
-          ...(sync ? { Counter1: { reset: null } } : {}),
-        }),
-    }),
+const mapStoreToProps = (dispatch, dispatchers) => ({
+  Counter1,
+  Counter2,
+  sync,
+}) => ({
+  ...(sync
+    ? {
+        Counter1: props =>
+          Counter1({
+            ...handlers(syncDispatch(dispatchers)),
+            ...props,
+          }),
+        Counter2: props =>
+          Counter2({
+            ...handlers(syncDispatch(dispatchers)),
+            ...props,
+          }),
+      }
+    : { Counter1, Counter2 }),
   toggleSync: () => dispatch({ sync: null }),
 })
 
-export default createModule(App)({ reducer, mapStoreToProps })
+const dependencies = {
+  Counter1: Counter,
+  Counter2: Counter,
+}
+
+export default createModule(App)({
+  reducer,
+  mapStoreToProps,
+  dependencies,
+})
